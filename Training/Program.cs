9c1f287c15d30183a51a7d045753a2f28f771aa9
MotyaMiracle <motyamiracle@gmail.com>
2023-01-08 19:34:38 +0300

    HttpResponse. Отправка ответа

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,21 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
-//app.MapGet("/", () => "Hello World!");
-
-//app.UseWelcomePage(); // подключение WelcomePageMiddleware
-//app.Run(HandleRequest);
-int x = 2;
-app.Run(async (context) =>
+app.Run(async (context) => 
 {
-    x = x * 2; // 2 * 2 = 4
-    await context.Response.WriteAsync($"Result: {x}");
+    var response = context.Response;
+    response.Headers.ContentLanguage = "ru-RU";
+    response.Headers.ContentType = "text/plain; charset=uft-8";
+    response.Headers.Append("secret-id", "256"); // добавление кастомного заголовка
+    await response.WriteAsync("Hello METANIT.COM", System.Text.Encoding.Default);
+    context.Response.StatusCode = 404;
+    await context.Response.WriteAsync("Resource Not Found");
+    response.ContentType = "text/html; charset=utf-8";
+    await response.WriteAsync("<h2>Hello METANIT.COM</h2><h3>Welcome to ASP.NET</h3>");
 });
-app.Run();
 
-//async Task HandleRequest(HttpContext context)
-//{
-//    await context.Response.WriteAsync("Hello METANIT.COM 2");
-//}
-// приложение запущено
-//app.Run(async (context) => await context.Response.WriteAsync("Hello METANIT.COM"));// в этой строке уже нет смысла
+app.Run();
