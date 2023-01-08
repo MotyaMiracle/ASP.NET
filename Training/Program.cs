be10ac5cad29d0de2bd88cca068333b110f508b4
MotyaMiracle <motyamiracle@gmail.com>
2023-01-08 20:33:14 +0300

    HttpRequest. Получение данных запроса

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,17 +1,48 @@
+using System.Runtime.InteropServices.ComTypes;
+
 var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
-app.Run(async (context) => 
+app.Run(async (context) =>
 {
+    context.Response.ContentType = "text/html; charset=utf-8";
+    var stringBuilder = new System.Text.StringBuilder("<table>");
+
+    foreach (var header in context.Request.Headers)
+    {
+        stringBuilder.Append($"<tr><td>{header.Key}</td><td>{header.Value}</td></tr>");
+    }
+    stringBuilder.Append("</table>");
+    await context.Response.WriteAsync(stringBuilder.ToString());
+    var acceptHeaderValue = context.Request.Headers.Accept;
+    //var acceptHeaderValue = context.Request.Headers["accept"];
+    await context.Response.WriteAsync($"Accept: {acceptHeaderValue}");
+    var path = context.Request.Path;
+    var now = DateTime.Now;
     var response = context.Response;
-    response.Headers.ContentLanguage = "ru-RU";
-    response.Headers.ContentType = "text/plain; charset=uft-8";
-    response.Headers.Append("secret-id", "256"); // добавление кастомного заголовка
-    await response.WriteAsync("Hello METANIT.COM", System.Text.Encoding.Default);
-    context.Response.StatusCode = 404;
-    await context.Response.WriteAsync("Resource Not Found");
-    response.ContentType = "text/html; charset=utf-8";
-    await response.WriteAsync("<h2>Hello METANIT.COM</h2><h3>Welcome to ASP.NET</h3>");
+
+    if (path == "/date")
+        await response.WriteAsync($"Date: {now.ToShortDateString()}");
+    else if (path == "/time")
+        await response.WriteAsync($"Time: {now.ToShortTimeString()}");
+    else
+        await response.WriteAsync("Hello METANIT.COM");
+
+    await context.Response.WriteAsync($"<p>Path: {context.Request.Path}</p>"
+        + $"<p>QueryString: {context.Request.QueryString}</p>");
+    stringBuilder = new System.Text.StringBuilder("<h3>Параметры строки запроса</h3><table>");
+    stringBuilder.Append("<tr><td>Параметр</td><td>Значение</td></tr>");
+    foreach (var param in context.Request.Query)
+    {
+        stringBuilder.Append($"<tr><td>{param.Key}</td><td>{param.Value}</td></tr>");
+    }
+    stringBuilder.Append("</table>");
+    await context.Response.WriteAsync(stringBuilder.ToString());
+    string name = context.Request.Query["name"]!;
+    string age = context.Request.Query["age"]!;
+    await context.Response.WriteAsync($"{name} - {age}");
+
+
 });
 
 app.Run();
