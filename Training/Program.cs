56b981364e9d01ca4c77df32b9a88f89aa84a957
MotyaMiracle <motyamiracle@gmail.com>
2023-01-10 13:04:11 +0300

    Отправка и получение json

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,18 +1,45 @@
 using Microsoft.Extensions.FileProviders;
+using System.ComponentModel;
 using System.Runtime.InteropServices.ComTypes;
+using System.Text.Json;
+using Training;
 
 var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
 app.Run(async (context) =>
 {
-    if (context.Request.Path == "/old")
-        //context.Response.Redirect("https://www.twitch.tv/"); // Но можно использовать редирект на внешние ресурсы
-        context.Response.Redirect("/new");
-    else if (context.Request.Path == "/new")
-        await context.Response.WriteAsync("New Page");
+    var response = context.Response;
+    var request = context.Request;
+    if(request.Path == "/api/user")
+    {
+        var responseText = "Неккоректные данные"; // Содержит сообщения по умолчанию
+        if (request.HasJsonContentType())
+        {
+            // определяем параметры сериализации/десериализации
+            var jsonoptions = new JsonSerializerOptions();
+            // добавляем конвертер кода json в объект типа Person
+            jsonoptions.Converters.Add(new PersonConverter());
+            // десериализуем данные с помощью конвертера PersonConverter
+            var person = await request.ReadFromJsonAsync<Person>(jsonoptions);
+            if (person != null)
+                responseText = $"Name: {person.Name}  Age: {person.Age}";
+        }
+        await response.WriteAsJsonAsync(new { text = responseText });
+    }
     else
-        await context.Response.WriteAsync("Main Page");
+    {
+        response.ContentType = "text/html; charset=utf-8";
+        await response.SendFileAsync("html/index.html");
+    }
+
+
+
+
+
+
+    //Person tom = new Person("Tom", 22);
+    //await context.Response.WriteAsJsonAsync(tom);
 });
 
 app.Run();
