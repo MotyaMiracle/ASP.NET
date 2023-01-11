1d99206b79460fb7c37c64bd253c7acd593c11f5
MotyaMiracle <motyamiracle@gmail.com>
2023-01-11 15:12:29 +0300

    Метод расширения для встраивания middleware

---
+++ Training/TokenExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Training
+{
+    public static class TokenExtensions
+    {
+        public static IApplicationBuilder UseToken(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<TokenMiddleware>();
+        }
+    }
+}
