517d932a7112ef36a5d53106098df51d4a8df65f
MotyaMiracle <motyamiracle@gmail.com>
2023-01-11 17:07:31 +0300

    Определение своих состояний среды

--- Training/Program.cs
+++ Training/Program.cs
@@ -8,14 +8,16 @@ var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
 //app.Environment.EnvironmentName = "Production"; // Программная установка значения среды
+// Можно и в файле launchSettings.json
+app.Environment.EnvironmentName = "Test"; // изменяем название среды на Test
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsEnvironment("Test")) // Если проект в состоянии "Test"
 {
-    app.Run(async (context) => await context.Response.WriteAsync("In Development Stage"));
+    app.Run(async (context) => await context.Response.WriteAsync("In Test Stage"));
 }
 else
 {
-    app.Run(async (context) => await context.Response.WriteAsync("In Production Stage"));
+    app.Run(async (context) => await context.Response.WriteAsync("In Development or Production Stage"));
 }
 Console.WriteLine($"{app.Environment.EnvironmentName}");
 
