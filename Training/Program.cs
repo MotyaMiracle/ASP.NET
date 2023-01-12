7fd20992a8cba1e56f3b88e0629ce466b054ecf6
MotyaMiracle <motyamiracle@gmail.com>
2023-01-12 17:53:45 +0300

    AddScoped

--- Training/Program.cs
+++ Training/Program.cs
@@ -3,8 +3,8 @@ using Training;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddTransient<ICounter, RandomCounter>();
-builder.Services.AddTransient<CounterService>();
+builder.Services.AddScoped<ICounter, RandomCounter>();
+builder.Services.AddScoped<CounterService>();
 
 
 var app = builder.Build();
