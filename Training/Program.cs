edfb7faa7fee51acc139348be5f55574606907d3
MotyaMiracle <motyamiracle@gmail.com>
2023-01-12 17:07:17 +0300

    Метод Invoke/InvokeAsync компонентов middleware

--- Training/Program.cs
+++ Training/Program.cs
@@ -4,18 +4,20 @@ using Training;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<ITimeService, ShortTimeService>();
-builder.Services.AddTransient<TimeMessage>();
+//builder.Services.AddTransient<TimeMessage>(); //Через конструктор
 
 var app = builder.Build();
 
-app.Run(async context =>
-{
-    //var timeService = context.RequestServices.GetService<ITimeService>(); HttpContext.RequestServices
-    //var timeService = app.Services.GetService<ITimeService>();// Свойство Services объекта WebApplication
-    //await context.Response.WriteAsync($"Time: {timeService?.GetTime()}");
-    var timeMessage = context.RequestServices.GetService<TimeMessage>();
-    context.Response.ContentType = "text/html; charset=utf-8";
-    await context.Response.WriteAsync($"<h2>{timeMessage?.GetTime()}</h2>");
-});
+app.UseMiddleware<TimeMessageMiddleware>();
+
+//app.Run(async context =>
+//{
+//var timeService = context.RequestServices.GetService<ITimeService>(); HttpContext.RequestServices
+//var timeService = app.Services.GetService<ITimeService>();// Свойство Services объекта WebApplication
+//await context.Response.WriteAsync($"Time: {timeService?.GetTime()}"); 
+//var timeMessage = context.RequestServices.GetService<TimeMessage>(); //Через конструктор
+//context.Response.ContentType = "text/html; charset=utf-8";
+//await context.Response.WriteAsync($"<h2>{timeMessage?.GetTime()}</h2>");
+//});
 
 app.Run();
---
+++ Training/TimeMessageMiddleware.cs
@@ -0,0 +1,17 @@
+﻿namespace Training
+{
+    public class TimeMessageMiddleware
+    {
+        readonly RequestDelegate next;
+        public TimeMessageMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+        
+        public async Task InvokeAsync(HttpContext context, ITimeService timeService)
+        {
+            context.Response.ContentType = "text/html;charset=utf-8";
+            await context.Response.WriteAsync($"<h1>Time: {timeService.GetTime()}</h1>");
+        }
+    }
+}
