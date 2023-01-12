90eb801b0afd041d90e50aeacbdb456502cd6ec1
MotyaMiracle <motyamiracle@gmail.com>
2023-01-12 16:03:40 +0300

    Расширения для добавления сервисов

--- Training/Program.cs
+++ Training/Program.cs
@@ -4,14 +4,17 @@ using Training;
 var builder = WebApplication.CreateBuilder(args);
 
 //builder.Services.AddTransient<ITimeService, LongTimeService>();
-builder.Services.AddTransient<TimeService>();
+//builder.Services.AddTransient<TimeService>();
+builder.Services.AddTimeService();
 
 var app = builder.Build();
 
 app.Run(async context =>
 {
     //var timeService = app.Services.GetRequiredService<ITimeService>();
-    var timeService = app.Services.GetRequiredService<TimeService>();
+    //var timeService = app.Services.GetRequiredService<TimeService>();
+    var timeService = app.Services.GetService<TimeService>();
+    context.Response.ContentType = "text/html; charset=utf-8";
     await context.Response.WriteAsync($"Time: {timeService?.GetTime()}");
 });
 
---
+++ Training/ServiceProviderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Training
+{
+    public static class ServiceProviderExtensions
+    {
+        public static void AddTimeService(this IServiceCollection services)
+        {
+            services.AddTransient<TimeService>();
+        }
+    }
+}
