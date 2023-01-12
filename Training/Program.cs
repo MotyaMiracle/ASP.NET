3d7e20546059392f46c1518b9d1dc28e712b69d4
MotyaMiracle <motyamiracle@gmail.com>
2023-01-12 16:51:01 +0300

    Конструкторы

--- Training/Program.cs
+++ Training/Program.cs
@@ -4,14 +4,18 @@ using Training;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<ITimeService, ShortTimeService>();
+builder.Services.AddTransient<TimeMessage>();
 
 var app = builder.Build();
 
 app.Run(async context =>
 {
-    //var timeService = context.RequestServices.GetService<ITimeService>(); Можно так
-    var timeService = app.Services.GetService<ITimeService>();
-    await context.Response.WriteAsync($"Time: {timeService?.GetTime()}");
+    //var timeService = context.RequestServices.GetService<ITimeService>(); HttpContext.RequestServices
+    //var timeService = app.Services.GetService<ITimeService>();// Свойство Services объекта WebApplication
+    //await context.Response.WriteAsync($"Time: {timeService?.GetTime()}");
+    var timeMessage = context.RequestServices.GetService<TimeMessage>();
+    context.Response.ContentType = "text/html; charset=utf-8";
+    await context.Response.WriteAsync($"<h2>{timeMessage?.GetTime()}</h2>");
 });
 
 app.Run();
---
+++ Training/TimeMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace Training
+{
+    public class TimeMessage
+    {
+        ITimeService timeService;
+        public TimeMessage(ITimeService timeService)
+        {
+            this.timeService = timeService;
+        }
+        public string GetTime() => $"Time: {timeService.GetTime()}";
+
+    }
+}
