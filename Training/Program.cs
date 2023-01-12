12793ebeb6bdfd432e0b08f1352bf29539b5bfb1
MotyaMiracle <motyamiracle@gmail.com>
2023-01-12 15:48:23 +0300

    Регистрация встроенных сервисов ASP.NET Core

--- Training/Program.cs
+++ Training/Program.cs
@@ -4,26 +4,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services; // Коллекция сервисов
 
+builder.Services.AddMvc();
+
 var app = builder.Build();
 
-app.Run(async (context) =>
-{
-    var sb = new StringBuilder();
-    sb.Append("<h1>Все сервисы</h1>");
-    sb.Append("<table>");
-    sb.Append("<tr><th>Тип</th><th>Lifetime</th><th>Реализация</th></tr>");
-    foreach (var svc in services)
-    {
-        sb.Append("<tr>");
-        sb.Append($"<td>{svc.ServiceType.FullName}</td>");
-        sb.Append($"<td>{svc.Lifetime}</td>");
-        sb.Append($"<td>{svc.ImplementationType?.FullName}</td>");
-        sb.Append("</tr>");
-    }
-    sb.Append("</table>");
-    context.Response.ContentType = "text/html;charset=utf-8";
-    await context.Response.WriteAsync(sb.ToString());
-
-});
 
 app.Run();
