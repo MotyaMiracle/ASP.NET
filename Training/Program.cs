f0166e8f0fffe84c726a5e9d9dddbe49c9444dd2
MotyaMiracle <MotyaMiracle@gmail.com>
2023-01-17 18:44:21 +0300

    Map and Use

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,6 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("First middleware starts");
+    await next.Invoke();
+    Console.WriteLine("First middleware ends");
+});
+
+app.Map("/", () =>
+{
+    Console.WriteLine("Index endpoint starts and end");
+    return "Index Page";
+});
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("Second middleware starts");
+    await next.Invoke();
+    Console.WriteLine("Second middleware ends");
+});
+
+app.Map("/about", () =>
+{
+    Console.WriteLine("About endpoint starts and ends");
+    return "About Page";
+});
 
 app.Run();
