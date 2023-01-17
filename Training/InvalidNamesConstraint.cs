6d5d83761af713da622f11f70d35458f60ac343d
MotyaMiracle <MotyaMiracle@gmail.com>
2023-01-17 17:30:49 +0300

    Создание ограничений маршрутов

---
+++ Training/InvalidNamesConstraint.cs
@@ -0,0 +1,13 @@
+﻿namespace Training
+{
+    public class InvalidNamesConstraint : IRouteConstraint
+    {
+        string[] names = new[] { "Tom", "Bob", "Sam" };
+
+        public bool Match(HttpContext? httpContext, IRouter? route, string routeKey,
+            RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            return !names.Contains(values[routeKey]?.ToString());
+        }
+    }
+}
--- Training/Program.cs
+++ Training/Program.cs
@@ -1,6 +1,23 @@
+using Training;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// проецируем класс SecretCodeConstraint на inline-ограничение secretcode
+builder.Services.Configure<RouteOptions>(options =>
+            options.ConstraintMap.Add("secretcode", typeof(SecretCodeConstraint)));
+
+
+// альтернативное добавление класса ограничения
+// builder.Services.AddRouting(options =>
+//              options.ConstraintMap.Add("secretcode", typeof(SecretConstraint)));
+builder.Services.AddRouting(options =>
+                options.ConstraintMap.Add("invalidnames", typeof(InvalidNamesConstraint)));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Map("/users/{name}/{token:secretcode(123456)}/", (string name, int token) =>
+            $"Name: {name} \nToken: {token}");
+app.Map("/users/{name:invalidnames}", (string name) => $"Name: {name}");
+app.Map("/", () => "Index Page");
 
 app.Run();
