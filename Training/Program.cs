3bc29357a24d92667b0eb696f36052325d53d65f
MotyaMiracle <MotyaMiracle@gmail.com>
2023-01-17 18:29:43 +0300

    Сопоставление запроса с конечной точкой

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.Map("/hello", () => "Hello METANIT.COM");
+//app.Map("/{message?}", (string? message) => $"Message: {message}");
+app.Map("/{controller}/Index/5", (string controller) => $"Controller: {controller}");
+app.Map("/Home/{action}/{id}", (string action) => $"Action: {action}");
+
+app.Map("/", () => "Index Page");
 
 app.Run();
