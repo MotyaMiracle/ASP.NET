329d9f6a02de49c1fa9536c18a2f5a0a708b03ce
MotyaMiracle <MotyaMiracle@gmail.com>
2023-01-17 15:06:27 +0300

    Параметры маршрута

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Ќеоб€зательно ставить разделитель /, можно -- или and
+//app.Map("/users/{id}/{name}", (string id, string name) => $"User id: {id}  User Name:{name}");
+app.Map("/users/{id}/{name}", HandleRequest);
+app.Map("/cars/{id?}", (string? id) => $"Car Id: {id ?? "Undefined"}");
+app.Map("{controller=Home}/{action=Index}/{id?}", 
+    (string controller, string action, string? id) =>
+    $"Controller: {controller} \nAction: {action} \nId: {id}");
+app.Map("products/{**info}", (string info) => $"Product Info: {info}");
+app.Map("/users", () => "Users Page");
+app.Map("/", () => "Index Page");
 
 app.Run();
+
+string HandleRequest(string id, string name)
+{
+    return $"User id: {id}  User Name:{name}";
+}
