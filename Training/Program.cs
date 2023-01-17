d666f3970eaade506b4db666c546676be57864bb
MotyaMiracle <MotyaMiracle@gmail.com>
2023-01-17 18:45:40 +0300

    context.Request.Path

--- Training/Program.cs
+++ Training/Program.cs
@@ -3,28 +3,13 @@ var app = builder.Build();
 
 app.Use(async (context, next) =>
 {
-    Console.WriteLine("First middleware starts");
-    await next.Invoke();
-    Console.WriteLine("First middleware ends");
+    if (context.Request.Path == "/date")
+        await context.Response.WriteAsync($"Date: {DateTime.Now.ToShortDateString()}");
+    else
+        await next.Invoke();
 });
 
-app.Map("/", () =>
-{
-    Console.WriteLine("Index endpoint starts and end");
-    return "Index Page";
-});
-
-app.Use(async (context, next) =>
-{
-    Console.WriteLine("Second middleware starts");
-    await next.Invoke();
-    Console.WriteLine("Second middleware ends");
-});
-
-app.Map("/about", () =>
-{
-    Console.WriteLine("About endpoint starts and ends");
-    return "About Page";
-});
+app.Map("/", () => "Index Page");
+app.Map("/about", () => "About Page");
 
 app.Run();
