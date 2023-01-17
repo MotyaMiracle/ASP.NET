0ca3cc89c15934bcf2c93df334b18cb7096f369f
MotyaMiracle <MotyaMiracle@gmail.com>
2023-01-17 15:24:14 +0300

    Ограничения маршрутов

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Map(
+    "/users/{name:alpha:minlength(2)}/{age:int:range(1, 110)}",
+    (string name, int age) => $"User Age: {age} \nUser Name:{name}"
+);
+app.Map(
+    "/phonebook/{phone:regex(^7-\\d{{3}}-\\d{{3}}-\\d{{4}}$)}/",
+    (string phone) => $"Phone: {phone}"
+);
+app.Map("/", () => "Index Page");
 
 app.Run();
