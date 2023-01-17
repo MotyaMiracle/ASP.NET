2bc59158dbbafa1c777ea006bb974cd01dd1a548
MotyaMiracle <MotyaMiracle@gmail.com>
2023-01-17 18:47:36 +0300

    Run()

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,14 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Use(async (context, next) =>
-{
-    await next.Invoke();
 
-    if (context.Response.StatusCode == 404)
-        await context.Response.WriteAsync("Resource Not Found");
-});
 app.Map("/", () => "Index Page");
 app.Map("/about", () => "About Page");
+
+app.Run(async context =>
+{
+    context.Response.StatusCode = 404;
+    await context.Response.WriteAsync("Resource not found");
+});
 
 app.Run();
