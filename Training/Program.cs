312f4a2ca527c2f2a4f11fca9bc569d02f423fad
MotyaMiracle <MotyaMiracle@gmail.com>
2023-01-18 22:12:29 +0300

    Метод UseDirectoryBrowser

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,13 +1,21 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 
-DefaultFilesOptions options = new DefaultFilesOptions();
-options.DefaultFileNames.Clear();
-options.DefaultFileNames.Add("hello.html");
+//DefaultFilesOptions options = new DefaultFilesOptions();
+//options.DefaultFileNames.Clear();
+//options.DefaultFileNames.Add("hello.html");
 
-app.UseDefaultFiles(options);
+//app.UseDefaultFiles(options);
 //app.UseDefaultFiles(); // поддержка страниц html по умолчанию
+
+app.UseDirectoryBrowser(new DirectoryBrowserOptions()
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\html")),
+    RequestPath = new PathString("/pages")
+});
 app.UseStaticFiles(); // добавляем поддержку статических файлов
 
 app.Run(async context => await context.Response.WriteAsync("Hello World"));
