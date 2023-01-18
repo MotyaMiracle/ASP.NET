50834f8a1eea4754fe1f6854c0e79916ef047b81
MotyaMiracle <MotyaMiracle@gmail.com>
2023-01-18 21:56:55 +0300

    Установка каталога статических файлов. UseStaticFiles

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,6 +1,9 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(
+    new WebApplicationOptions { WebRootPath = "static"});
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles(); // добавляем поддержку статических файлов
+
+app.Run(async context => await context.Response.WriteAsync("Hello World"));
 
 app.Run();
