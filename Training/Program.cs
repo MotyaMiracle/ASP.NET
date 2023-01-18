6e02f6b3321ca35265251402e4374e68c6f08c05
MotyaMiracle <MotyaMiracle@gmail.com>
2023-01-18 22:01:28 +0300

    Файлы по умолчанию

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,13 @@
-var builder = WebApplication.CreateBuilder(
-    new WebApplicationOptions { WebRootPath = "static"});
+var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
+DefaultFilesOptions options = new DefaultFilesOptions();
+options.DefaultFileNames.Clear();
+options.DefaultFileNames.Add("hello.html");
+
+app.UseDefaultFiles(options);
+//app.UseDefaultFiles(); // поддержка страниц html по умолчанию
 app.UseStaticFiles(); // добавляем поддержку статических файлов
 
 app.Run(async context => await context.Response.WriteAsync("Hello World"));
