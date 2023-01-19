19b74534b80145b629f677b646a5f4c6955b08c6
MotyaMiracle <motyamiracle@gmail.com>
2023-01-19 20:09:17 +0300

    Конфигурация по умолчанию и объединение конфигурации

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,24 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
-
-//builder.Configuration.AddInMemoryCollection(new Dictionary<string, string>
-//{
-//    {"name", "Tom" },
-//    {"age", "37" }
-//}!);
-
 var app = builder.Build();
 
-// установка настроек конфигурации
-app.Configuration["name"] = "Tom";
-app.Configuration["age"] = "37";
+builder.Configuration
+    .AddJsonFile("config.json")
+    .AddXmlFile("config.xml")
+    .AddIniFile("config.ini")
+    .AddInMemoryCollection(new Dictionary<string, string>
+{
+    { "name", "Sam"},
+    { "age", "32"}
+}); ;
 
-//app.Run(async context =>
-//{
-//    // получение настроек конфигурации
-//    string name = app.Configuration["name"];
-//    string age = app.Configuration["age"];
-//    await context.Response.WriteAsync($"{name} - {age}");
-//});
 app.Map("/", (IConfiguration appConfig) => $"{appConfig["name"]} - {appConfig["age"]}");
 
 app.Run();
