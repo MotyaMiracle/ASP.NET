3c2c34584bd93d18931afc6386acb91d24184c55
MotyaMiracle <motyamiracle@gmail.com>
2023-01-19 19:47:33 +0300

    Хранение конфигурации в памяти

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,10 +1,23 @@
 //string[] commandLineArgs = { "name=Alice", "age=29" }; // псевдопараметры командной строки
 var builder = WebApplication.CreateBuilder();
-string[] commandLineArgs = { "name=Sam", "age=25" };
-builder.Configuration.AddCommandLine(commandLineArgs); // передаем параметры в качестве конфигурации
+//string[] commandLineArgs = { "name=Sam", "age=25" };
+//builder.Configuration.AddCommandLine(commandLineArgs); // передаем параметры в качестве конфигурации
 
 var app = builder.Build();
 
-app.Map("/", (IConfiguration appConfig) => $"{appConfig["name"]} - {appConfig["age"]}");
+builder.Configuration.AddInMemoryCollection(new Dictionary<string, string>
+{
+    {"name", "Tom"},
+    {"age", "37"}
+});
+
+app.Map("/", (IConfiguration appConfig) =>
+{
+    var name = appConfig["name"];
+    var age = appConfig["age"];
+    return $"{name} - {age}";
+});
+
+//app.Map("/", (IConfiguration appConfig) => $"{appConfig["name"]} - {appConfig["age"]}");
 
 app.Run();
