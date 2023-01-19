a4bb47bb0150aa144ef28f667bd8ce9f0a6e5fe4
MotyaMiracle <motyamiracle@gmail.com>
2023-01-19 19:14:46 +0300

    Получение данных конфигурации

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// установка настроек конфигурации
+app.Configuration["name"] = "Tom";
+app.Configuration["age"] = "37";
+
+app.Run(async context =>
+{
+    // получение настроек конфигурации
+    string name = app.Configuration["name"];
+    string age = app.Configuration["age"];
+    await context.Response.WriteAsync($"{name} - {age}");
+});
 
 app.Run();
