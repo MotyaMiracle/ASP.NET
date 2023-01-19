9e6b1e84ce52cc2137e872b8d10525fc93c6e57b
MotyaMiracle <motyamiracle@gmail.com>
2023-01-19 19:57:12 +0300

    Конфигурация в JSON

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,24 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Configuration.AddInMemoryCollection(new Dictionary<string, string>
-//{
-//    {"name", "Tom" },
-//    {"age", "37" }
-//}!);
 
 var app = builder.Build();
 
-// установка настроек конфигурации
-app.Configuration["name"] = "Tom";
-app.Configuration["age"] = "37";
+builder.Configuration
+    .AddJsonFile("config.json");
+    //.AddJsonFile("otherconfig.json");
 
-//app.Run(async context =>
-//{
-//    // получение настроек конфигурации
-//    string name = app.Configuration["name"];
-//    string age = app.Configuration["age"];
-//    await context.Response.WriteAsync($"{name} - {age}");
-//});
-app.Map("/", (IConfiguration appConfig) => $"{appConfig["name"]} - {appConfig["age"]}");
+app.Map("/", (IConfiguration appConfig) =>
+{
+    var personName = appConfig["person:profile:name"];
+    var companyName = appConfig["company:name"];
+    return $"{personName} - {companyName}";
+});
+
 
 app.Run();
