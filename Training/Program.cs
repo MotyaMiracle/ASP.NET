5cf9f18e37d6daf5b3803b03b1a3afec70c93f58
MotyaMiracle <motyamiracle@gmail.com>
2023-01-19 19:46:13 +0300

    Применение метода AddCommandLine

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,6 +1,7 @@
-string[] commandLineArgs = { "name=Alice", "age=29" }; // псевдопараметры командной строки
-var builder = WebApplication.CreateBuilder(commandLineArgs);
-
+//string[] commandLineArgs = { "name=Alice", "age=29" }; // псевдопараметры командной строки
+var builder = WebApplication.CreateBuilder();
+string[] commandLineArgs = { "name=Sam", "age=25" };
+builder.Configuration.AddCommandLine(commandLineArgs); // передаем параметры в качестве конфигурации
 
 var app = builder.Build();
 
