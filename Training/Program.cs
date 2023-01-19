cbd2cb065d38ef0753b10791f8dadfea6da8f98b
MotyaMiracle <motyamiracle@gmail.com>
2023-01-19 19:59:05 +0300

    Конфигурация в XML

--- Training/Program.cs
+++ Training/Program.cs
@@ -3,9 +3,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-builder.Configuration
-    .AddJsonFile("config.json");
-    //.AddJsonFile("otherconfig.json");
+//builder.Configuration
+//    .AddJsonFile("config.json");
+//.AddJsonFile("otherconfig.json");
+
+//app.Map("/", (IConfiguration appConfig) =>
+//{
+//    var personName = appConfig["person:profile:name"];
+//    var companyName = appConfig["company:name"];
+//    return $"{personName} - {companyName}";
+//});
+builder.Configuration.AddXmlFile("config.xml");
 
 app.Map("/", (IConfiguration appConfig) =>
 {
