b1752b195e22da8b49d092010a50198ccc1cfccf
MotyaMiracle <motyamiracle@gmail.com>
2023-01-20 12:44:27 +0300

    Через механизм Dependency Injection

--- Training/Program.cs
+++ Training/Program.cs
@@ -4,10 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 builder.Configuration.AddJsonFile("person.json");
-var tom = new Person();
-app.Configuration.Bind(tom);
+//var tom = new Person();
+//Person tom = app.Configuration.Get<Person>(); // можно так чтобы не создвать предварительно объект класса
+//app.Configuration.Bind(tom);
+//app.Run(async context => await context.Response.WriteAsync($"{tom.Name} - {tom.Age}"));
 
-app.Run(async context => await context.Response.WriteAsync($"{tom.Name} - {tom.Age}"));
+app.Map("/", (IConfiguration appConfig) =>
+{
+    var tom = app.Configuration.Get<Person>(); // связываем конфигурацию с объектом tom
+    return $"{tom.Name} - {tom.Age}";
+});
 
 
 app.Run();
