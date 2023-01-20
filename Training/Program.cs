5f3a45d22fb1d15c466209d240466fc53f3632c9
MotyaMiracle <motyamiracle@gmail.com>
2023-01-20 12:59:17 +0300

    Привязка конфигурации из xml

--- Training/Program.cs
+++ Training/Program.cs
@@ -3,7 +3,7 @@ using Training;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-builder.Configuration.AddJsonFile("person.json");
+//builder.Configuration.AddJsonFile("person.json");
 //var tom = new Person();
 //Person tom = app.Configuration.Get<Person>(); // можно так чтобы не создвать предварительно объект класса
 //app.Configuration.Bind(tom);
@@ -14,6 +14,26 @@ builder.Configuration.AddJsonFile("person.json");
 //    var tom = app.Configuration.Get<Person>(); // связываем конфигурацию с объектом tom
 //    return $"{tom.Name} - {tom.Age}";
 //});
+
+//var tom = new Person();
+//app.Configuration.Bind(tom);
+
+//app.Run(async context =>
+//{
+//    context.Response.ContentType = "text/html; charset=utf-8";
+//    string name = $"<p>Name: {tom.Name}</p>";
+//    string age = $"<p>Age: {tom.Age}</p>";
+//    string company = $"<p>Company: {tom.Company?.Title}</p>";
+//    string langs = $"<p>Languages:</p><ul>";
+//    foreach (var lang in tom.Languages)
+//    {
+//        langs += $"<li><p>{lang}</p></li>";
+//    }
+//    langs += "</ul>";
+//    await context.Response.WriteAsync($"{name}{age}{company}{langs}");
+//});
+
+builder.Configuration.AddXmlFile("person.xml");
 
 var tom = new Person();
 app.Configuration.Bind(tom);
