0a32b27b7b6e13fef1729f63399db2f04e7716d7
MotyaMiracle <motyamiracle@gmail.com>
2023-01-20 12:41:49 +0300

    Bind()

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,24 +1,13 @@
+using Training;
+
 var builder = WebApplication.CreateBuilder(args);
-
-//builder.Configuration.AddInMemoryCollection(new Dictionary<string, string>
-//{
-//    {"name", "Tom" },
-//    {"age", "37" }
-//}!);
-
 var app = builder.Build();
 
-// установка настроек конфигурации
-app.Configuration["name"] = "Tom";
-app.Configuration["age"] = "37";
+builder.Configuration.AddJsonFile("person.json");
+var tom = new Person();
+app.Configuration.Bind(tom);
 
-//app.Run(async context =>
-//{
-//    // получение настроек конфигурации
-//    string name = app.Configuration["name"];
-//    string age = app.Configuration["age"];
-//    await context.Response.WriteAsync($"{name} - {age}");
-//});
-app.Map("/", (IConfiguration appConfig) => $"{appConfig["name"]} - {appConfig["age"]}");
+app.Run(async context => await context.Response.WriteAsync($"{tom.Name} - {tom.Age}"));
+
 
 app.Run();
