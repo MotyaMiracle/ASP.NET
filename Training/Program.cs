c7e4dfa879cd2446967cb14304a0c26dcdaef427
MotyaMiracle <MotyaMiracle@gmail.com>
2023-01-25 16:46:48 +0300

    Конфигурация и фильтрация логгирования

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,30 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 var app = builder.Build();
 
-// Получение логгера через внедрение зависимостей
-//app.Map("/hello", (ILogger<Program> logger) =>
-//{
-//    logger.LogInformation($"Path: /hello Time: {DateTime.Now.ToLongTimeString()}");
-//    return "Hello World!";
-//});
 
-// Ведение лога и ILogger
-//app.Run(async context =>
-//{
-//    // пишем на консоль информацию
-//    app.Logger.LogInformation($"Processing request: {context.Request.Path}");
-
-//    await context.Response.WriteAsync("Hello World!");
-//});
-// Уровни и методы логгирования
-app.Run(async (context) =>
-{
-    var path = context.Request.Path;
-    app.Logger.LogCritical($"LogCritical {path}");
-    app.Logger.LogError($"LogError {path}");
-    app.Logger.LogInformation($"LogInformation {path}");
-    app.Logger.LogWarning($"LogWarning {path}");
-
-    await context.Response.WriteAsync("Hello World!");
-});
 app.Run();
