5296dfdc5d6f9dd5e0e2d540e0d6ad555b175f84
MotyaMiracle <MotyaMiracle@gmail.com>
2023-01-25 16:20:29 +0300

    Логгирование

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,6 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Получение логгера через внедрение зависимостей
+//app.Map("/hello", (ILogger<Program> logger) =>
+//{
+//    logger.LogInformation($"Path: /hello Time: {DateTime.Now.ToLongTimeString()}");
+//    return "Hello World!";
+//});
 
+// Ведение лога и ILogger
+//app.Run(async context =>
+//{
+//    // пишем на консоль информацию
+//    app.Logger.LogInformation($"Processing request: {context.Request.Path}");
+
+//    await context.Response.WriteAsync("Hello World!");
+//});
+// Уровни и методы логгирования
+app.Run(async (context) =>
+{
+    var path = context.Request.Path;
+    app.Logger.LogCritical($"LogCritical {path}");
+    app.Logger.LogError($"LogError {path}");
+    app.Logger.LogInformation($"LogInformation {path}");
+    app.Logger.LogWarning($"LogWarning {path}");
+
+    await context.Response.WriteAsync("Hello World!");
+});
 app.Run();
