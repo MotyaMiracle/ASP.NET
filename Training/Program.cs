5f9e403a6814123170068921ca51d23d8d08ffab
MotyaMiracle <motyamiracle@gmail.com>
2023-01-26 16:04:47 +0300

    HttpContext.Items

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Use(async (context, next) =>
+{
+    //context.Items["text"] = "Hello from HttpContext.Items";
+    context.Items.Add("message", "Hello METANIT.COM");
+    await next.Invoke();
+});
+
+app.Run(async context => {
+
+    if (context.Items.ContainsKey("message"))
+        await context.Response.WriteAsync($"Message: {context.Items["message"]}");
+    else
+        await context.Response.WriteAsync("Random Text");
+});
 
 app.Run();
