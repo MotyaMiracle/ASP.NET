7f7f6d8d04c41bdb3fc754c5cf90f5153696c638
MotyaMiracle <motyamiracle@gmail.com>
2023-01-26 16:13:27 +0300

    Куки

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async context =>
+{
+    //if (context.Request.Cookies.ContainsKey("name"))
+    //{
+    //    string? name = context.Request.Cookies["name"];
+    //    await context.Response.WriteAsync($"Hello {name}!");
+    //}
+    // TryGetValue()
+    if(context.Request.Cookies.TryGetValue("name", out var login))
+    {
+        await context.Response.WriteAsync($"Hello {login}!");
+    }
+    else
+    {
+        context.Response.Cookies.Append("name", "Tom");
+        await context.Response.WriteAsync("Hello World!");
+    }
+});
 
 app.Run();
