fa63757d37fabbd171222b465e949fe95f594b15
MotyaMiracle <motyamiracle@gmail.com>
2023-01-26 16:29:35 +0300

    Сессии

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDistributedMemoryCache(); // add IDistributedMemoryCache
+builder.Services.AddSession(); // adding session services
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSession(); // adding middleware to work with sessions
+
+app.Run(async context =>
+{
+    if (context.Session.Keys.Contains("name"))
+        await context.Response.WriteAsync($"Hello {context.Session.GetString("name")}");
+    else
+    {
+        context.Session.SetString("name", "Tom");
+        await context.Response.WriteAsync("Hello World!");
+    }
+});
+
+
 
 app.Run();
