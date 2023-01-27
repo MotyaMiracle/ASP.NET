57005a4045cf6d9d608a1fdf035eb81c3c8f2cc6
MotyaMiracle <motyamiracle@gmail.com>
2023-01-27 12:23:08 +0300

    UseStatusCodePages()

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Handling HTTP Errors
+/*The UseStatusCodePages() method should be called closer to the beginning of the request 
+ * processing pipeline, in particular, before adding middleware for working with static files 
+ * and before adding endpoints.*/
+app.UseStatusCodePages();
+
+app.Map("/hello", () => "Hello ASP.NET Core");
 
 app.Run();
