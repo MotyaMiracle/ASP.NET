ae06df9a5d7c17fe07e8df910174ebd4a20c03c2
MotyaMiracle <motyamiracle@gmail.com>
2023-01-27 12:11:26 +0300

    UseDeveloperExceptionPage

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.UseDeveloperExceptionPage();
+app.Environment.EnvironmentName = "Production"; // change Enviroment
+
+app.Run(async context =>
+{
+    int a = 5;
+    int b = 0;
+    int c = a / b;
+    await context.Response.WriteAsync($"c = {c}");
+});
 
 app.Run();
