fe7d4f413e16a66edf548b767f1738ffa8da8feb
MotyaMiracle <motyamiracle@gmail.com>
2023-01-27 12:44:44 +0300

    Методы UseStatusCodePagesWithRedirects и UseStatusCodePagesWithReExecute

--- Training/Program.cs
+++ Training/Program.cs
@@ -7,22 +7,29 @@ var app = builder.Build();
  * and before adding endpoints.*/
 //app.UseStatusCodePages("text/plain", "Error: Resource Not Found. Status code: {0}");
 
-app.UseStatusCodePages(async statusCodeContext =>
-{
-    var response = statusCodeContext.HttpContext.Response;
-    var path = statusCodeContext.HttpContext.Request.Path;
+//app.UseStatusCodePages(async statusCodeContext =>
+//{
+//    var response = statusCodeContext.HttpContext.Response;
+//    var path = statusCodeContext.HttpContext.Request.Path;
 
-    response.ContentType = "text/plain; charset=UTF-8";
-    if (response.StatusCode == 403)
-    {
-        await response.WriteAsync($"Path: {path}. Access denied");
-    }
-    else if (response.StatusCode == 404)
-    {
-        await response.WriteAsync($"Path: {path}. Not found");
-    }
-});
+//    response.ContentType = "text/plain; charset=UTF-8";
+//    if (response.StatusCode == 403)
+//    {
+//        await response.WriteAsync($"Path: {path}. Access denied");
+//    }
+//    else if (response.StatusCode == 404)
+//    {
+//        await response.WriteAsync($"Path: {path}. Not found");
+//    }
+//});
+
+//app.UseStatusCodePagesWithRedirects("/error/{0}");
+//app.UseStatusCodePagesWithReExecute("/error/{0}");
+//Second parameter accepts query string parameters
+app.UseStatusCodePagesWithReExecute("/error", "?code={0}");
 
 app.Map("/hello", () => "Hello ASP.NET Core");
+app.Map("/error", (string code) => $"Error Code: {code}");
+//app.Map("/error/{statusCode}", (int statusCode) => $"Error. Status Code: {statusCode}");
 
 app.Run();
