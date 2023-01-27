7bce75873798f321c5f3c3adc5969d0fb571caf9
MotyaMiracle <motyamiracle@gmail.com>
2023-01-27 12:32:28 +0300

    Настройка сообщения

--- Training/Program.cs
+++ Training/Program.cs
@@ -5,7 +5,7 @@ var app = builder.Build();
 /*The UseStatusCodePages() method should be called closer to the beginning of the request 
  * processing pipeline, in particular, before adding middleware for working with static files 
  * and before adding endpoints.*/
-app.UseStatusCodePages();
+app.UseStatusCodePages("text/plain", "Error: Resource Not Found. Status code: {0}");
 
 app.Map("/hello", () => "Hello ASP.NET Core");
 
