976032101c54c928fb01be84fcc942fd9062b758
MotyaMiracle <motyamiracle@gmail.com>
2023-01-31 11:09:24 +0300

    Введение в Results API

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.Map("/hello", () => SendHello);
+//app.Map("/", () => "Hello ASP.NET Core");
+
+app.Run(async context =>
+{
+    await Results.Text("Hello ASP.NET Core").ExecuteAsync(context);
+});
 
 app.Run();
+
+IResult SendHello()
+{
+    return Results.Text("Hello ASP.NET Core");
+}
