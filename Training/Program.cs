96a1be967859b1819d92cf941204cd4820b6a032
MotyaMiracle <motyamiracle@gmail.com>
2023-01-31 12:37:35 +0300

    Отправка статусных кодов в Results API

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,14 +1,28 @@
-﻿using Microsoft.AspNetCore.ResponseCompression;
-
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// LocalRedirect
-//app.Map("/old", () => Results.LocalRedirect("/new"));
-//app.Map("/new", () => "New Address");
+// StatusCode
+app.Map("/about", () => Results.StatusCode(401));
 
-// Redirect
-app.Map("/old", () => Results.Redirect("https://metanit.com"));
+// NotFound
+app.Map("/contacts", () => Results.NotFound(new { message = "Resource Not Found" }));
+app.Map("/address", () => Results.NotFound("Error 404. Invalid address"));
+
+// Unauthorized
+app.Map("/acc", () => Results.Unauthorized());
 app.Map("/", () => "Hello World!");
+
+// BadRequest
+app.Map("/content/{age:int}", (int age) =>
+{
+    if (age < 18)
+        return Results.BadRequest(new { message = "Invalid age" });
+    else
+        return Results.Content("Access is available");
+});
+
+// Ok
+app.Map("/car", () => Results.Ok("Lamborgini"));
+app.Map("/access", () => Results.Ok(new { message = "Success!" }));
 
 app.Run();
