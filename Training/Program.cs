63c5d7fac6da7e76bb196ff155e7558b987c7f0d
MotyaMiracle <motyamiracle@gmail.com>
2023-01-31 12:11:23 +0300

    Отправка текста и json в Results API

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,17 +1,33 @@
+﻿using Microsoft.AspNetCore.ResponseCompression;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-//app.Map("/hello", () => SendHello);
-//app.Map("/", () => "Hello ASP.NET Core");
+// Send text and method Content
+//app.Map("/", () => Results.Content("你好", "text/plain", System.Text.Encoding.Unicode));
+//app.Map("/", () => Results.Content("Hello ASP.NET Core"));
 
-app.Run(async context =>
+// Text
+//app.Map("/chinese", () => Results.Text("你好", "text/plain", System.Text.Encoding.Unicode));
+//app.Map("/", () => Results.Text("Hello World"));
+
+// Send JSON
+//app.Map("/person", () => Results.Json(new Person("Bob", 41))); // send object Person
+//app.Map("/", () => Results.Json(new { Name = "Tom", Age = 32 })); // send anon object
+app.Map("/sam", () => Results.Json(new Person("Sam", 25), new()
 {
-    await Results.Text("Hello ASP.NET Core").ExecuteAsync(context);
-});
+    PropertyNameCaseInsensitive = false,
+    NumberHandling = System.Text.Json.Serialization.JsonNumberHandling.WriteAsString
+}));
 
+app.Map("/bob", () => Results.Json(new Person("Bob", 41), 
+    new(System.Text.Json.JsonSerializerDefaults.Web)));
+
+app.Map("/tom", () => Results.Json(new Person("Tom", 32),
+    new(System.Text.Json.JsonSerializerDefaults.General)));
 app.Run();
 
-IResult SendHello()
-{
-    return Results.Text("Hello ASP.NET Core");
-}
+app.Map("/error", () => Results.Json(new { message="Unexpected error" }, statusCode: 500));
+
+app.Map("/", () => "Hello World!");
+record class Person(string Name, int Age);
