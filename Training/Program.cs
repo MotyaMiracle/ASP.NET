b4cd02cf6646a61207ab5003412751eb47ceb340
MotyaMiracle <motyamiracle@gmail.com>
2023-02-01 13:11:52 +0300

    Пример приложения Web API

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,13 +1,15 @@
+using System.Xml.Linq;
 using Training;
-// начальные данные
+
+// initial data
 List<Person> users = new List<Person>
 {
-    new(){Id = Guid.NewGuid().ToString(), Name = "Tom", Age = 32},
-    new(){Id = Guid.NewGuid().ToString(), Name = "Bob", Age = 43},
-    new(){Id = Guid.NewGuid().ToString(), Name = "Sam", Age = 28}
+    new() { Id = Guid.NewGuid().ToString(), Name = "Tom", Age = 37 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Bob", Age = 41 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Sam", Age = 24 }
 };
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
 app.UseDefaultFiles();
@@ -15,31 +17,32 @@ app.UseStaticFiles();
 
 app.MapGet("/api/users", () => users);
 
-app.MapGet("/api/users/{id:string}", (string id) =>
+app.MapGet("/api/users/{id}", (string id) =>
 {
     // get user by id
-    Person? user = users.FirstOrDefault(x => x.Id == id);
+    Person? user = users.FirstOrDefault(u => u.Id == id);
     // if not found, send status code and error message
-    if (user == null)
-        return Results.NotFound(new { message = "Пользователь не найден" });
+    if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
+
     // if the user is found, send it
     return Results.Json(user);
 });
 
-app.MapDelete("/api/users/{id:string}", (string id) =>
+app.MapDelete("/api/users/{id}", (string id) =>
 {
     // get user by id
-    Person? user = users.FirstOrDefault(x => x.Id == id);
+    Person? user = users.FirstOrDefault(u => u.Id == id);
+
     // if not found, send status code and error message
-    if (user == null)
-        return Results.NotFound(new { message = "Пользователь не найден" });
-    // if the user is found, delete and send it
+    if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
+
+    // if the user is found, delete it
     users.Remove(user);
     return Results.Json(user);
 });
 
-app.MapPost("/api/users", (Person user) =>
-{
+app.MapPost("/api/users", (Person user) => {
+
     // set id for new user
     user.Id = Guid.NewGuid().ToString();
     // add the user to the list
@@ -47,15 +50,16 @@ app.MapPost("/api/users", (Person user) =>
     return user;
 });
 
-app.MapPut("/api/users", (Person userData) =>
-{
+app.MapPut("/api/users", (Person userData) => {
+
     // get user by id
     var user = users.FirstOrDefault(u => u.Id == userData.Id);
     // if not found, send status code and error message
     if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
     // if the user is found, change his data and send it back to the client
-    user.Name = userData.Name;
+
     user.Age = userData.Age;
+    user.Name = userData.Name;
     return Results.Json(user);
 });
 
