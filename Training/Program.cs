d44597bd8500fe1727b294b834e07af4dc5b40d9
MotyaMiracle <motyamiracle@gmail.com>
2023-02-02 14:33:08 +0300

    Введение в аутентификацию и авторизацию

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.AspNetCore.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// adding authentication services
+builder.Services.AddAuthentication("Bearer") // authentication scheme - using jwt tokens
+    .AddJwtBearer(); // connect authentication using jwt tokens
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseAuthentication(); // add authentication middleware
+app.UseAuthorization(); // adding authorization middleware
+
+app.Map("/hello", [Authorize] () => "Hello World!");
+app.Map("/", () => "Home Page");
 
 app.Run();
