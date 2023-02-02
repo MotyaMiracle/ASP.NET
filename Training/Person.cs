98e0c87e2166390b4b65397898f4e847589e5e31
MotyaMiracle <motyamiracle@gmail.com>
2023-02-03 00:37:22 +0300

    Авторизация с помощью JWT-токенов в клиенте JavaScript, но проблемы с подключением к серверу wss

---
+++ Training/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace Training
+{
+    public class Person
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
--- Training/Program.cs
+++ Training/Program.cs
@@ -5,6 +5,14 @@ using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Training;
 
+
+// conditional database with users
+var people = new List<Person>
+{
+    new Person{Email = "tom@gmail.com", Password = "12345"},
+    new Person{Email = "bob@gmail.com", Password = "55555"}
+};
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAuthorization(); // adding authorization services
@@ -32,12 +40,21 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 var app = builder.Build();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.UseAuthentication(); // add authentication middleware
 app.UseAuthorization(); // adding authorization middleware
 
-app.Map("/login/{username}", (string username) =>
+app.MapPost("/login", (Person loginData) =>
 {
-    var claims = new List<Claim> { new Claim(ClaimTypes.Name, username) };
+    // find the user
+    Person? person = people.FirstOrDefault(p => p.Email == loginData.Email && p.Password == loginData.Password);
+    // if user not found, send status code 401
+    if (person is null)
+        return Results.Unauthorized();
+
+    var claims = new List<Claim> { new Claim(ClaimTypes.Name, person.Email) };
     // create a JWT token
     var jwt = new JwtSecurityToken(
         issuer: AuthOptions.ISSUER,
@@ -46,11 +63,20 @@ app.Map("/login/{username}", (string username) =>
         expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
         signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), 
         SecurityAlgorithms.HmacSha256));
-    return new JwtSecurityTokenHandler().WriteToken(jwt);
+    var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+    // form the response
+    var response = new
+    {
+        access_token = encodedJwt,
+        username = person.Email
+    };
+    
+    return Results.Json(response);
 });
 
 
-app.Map("/data", [Authorize] () => new {message = "Hello World!" });
+app.Map("/data", [Authorize] () => $"Hello World!");
 
 
 app.Run();
