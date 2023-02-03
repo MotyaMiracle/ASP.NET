99fdd25a6cc38cc5f6b0e92c8bec38ead06e86e4
MotyaMiracle <motyamiracle@gmail.com>
2023-02-03 11:19:32 +0300

    HttpContext.User, ClaimPrincipal и ClaimsIdentity

--- Training/Program.cs
+++ Training/Program.cs
@@ -17,7 +17,7 @@ var people = new List<Person>
 };
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options => options.LoginPath = "/login");
+    .AddCookie();
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
@@ -27,62 +27,34 @@ app.UseAuthorization();
 
 app.MapGet("/login", async (HttpContext context) =>
 {
-    context.Response.ContentType = "text/html; charset=utf-8";
-    string loginForm = @"<!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset='utf-8' />
-        <title>METANIT.COM</title>
-    </head>
-    <body>
-        <h2>Login Form</h2>
-        <form method='post'>
-            <p>
-                <label>Email</label><br />
-                <input name='email' />
-            </p>
-            <p>
-                <label>Password</label><br />
-                <input type='password' name='password' />
-            </p>
-            <input type='submit' value='Login' />
-        </form>
-    </body>
-    </html>";
-    await context.Response.WriteAsync(loginForm);
-});
-
-app.MapPost("/login", async (string? returnUrl, HttpContext context) =>
-{
-    // get email and password from the form
-    var form = context.Request.Form;
-    // if email and/or password are not set, send a 400 error status code
-    if (!form.ContainsKey("email") || !form.ContainsKey("password"))
-        return Results.BadRequest("Email и/или пароль не установлены");
-
-    string email = form["email"];
-    string password = form["password"];
-
-    // find the user
-    Person? person = people.FirstOrDefault(p => p.Email == email && p.Password == password);
-    // if user not found, send status code 401
-    if (person is null) return Results.Unauthorized();
-
-    var claims = new List<Claim> { new Claim(ClaimTypes.Name, person.Email) };
-    // create a ClaimsIdentity object
-    ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+    var claimsIdentity = new ClaimsIdentity("Undefined");
+    var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
     // set authentication cookies
-    await context.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
-    return Results.Redirect(returnUrl ?? "/");
+    await context.SignInAsync(claimsPrincipal);
+    return Results.Redirect("/");
 });
 
 app.MapGet("/logout", async (HttpContext context) =>
 {
     await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-    return Results.Redirect("/login");
+    return "Данные удалены";
 });
 
-app.Map("/", [Authorize] () => $"Hello World!");
+app.Map("/", (HttpContext context) =>
+{
+    var user = context.User.Identity;
+    if(user is not null && user.IsAuthenticated)
+        return $"Пользователь аутентифицирован. Тип аутентификации: {user.AuthenticationType}";
+    else
+        return "Пользователь НЕ аутентифицирован";
+});
 
+app.Map("/", (ClaimsPrincipal claimsPrincipal) =>
+{
+    var user = claimsPrincipal.Identity;
+    if (user is not null && user.IsAuthenticated)
+        return "Пользователь аутентифицирован";
+    else return "Пользователь не аутентифицирован";
+});
 
 app.Run();
