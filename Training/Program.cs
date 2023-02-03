547296edcbff39b044485a3a2263d2b43b6762e0
MotyaMiracle <motyamiracle@gmail.com>
2023-02-03 12:20:10 +0300

    ClaimPrincipal и объекты Claim

--- Training/Program.cs
+++ Training/Program.cs
@@ -16,45 +16,16 @@ var app = builder.Build();
 
 app.UseAuthentication();
 
-// Add age
-app.MapGet("/addage", async (HttpContext context) =>
-{
-    if (context.User.Identity is ClaimsIdentity claimsIdentity)
-    {
-        claimsIdentity.AddClaim(new Claim("age", "37"));
-        var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
-        await context.SignInAsync(claimsPrincipal);
-    }
-    return Results.Redirect("/");
-});
-
-// delete phone
-app.MapGet("/removephone", async (HttpContext context) =>
-{
-    if (context.User.Identity is ClaimsIdentity claimsIdentity)
-    {
-        var phoneClaim = claimsIdentity.FindFirst(ClaimTypes.MobilePhone);
-        // if the claim is successfully deleted
-        if (claimsIdentity.TryRemoveClaim(phoneClaim))
-        {
-            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
-            await context.SignInAsync(claimsPrincipal);
-        }
-    }
-    return Results.Redirect("/");
-});
 
 app.MapGet("/login", async (HttpContext context) =>
 {
-    var username = "Tom";
-    var company = "Microsoft";
-    var phone = "+12345678901";
 
     var claims = new List<Claim>
     {
-        new Claim(ClaimTypes.Name, username),
-        new Claim("company", company),
-        new Claim(ClaimTypes.MobilePhone, phone)
+        new Claim(ClaimTypes.Name, "Tom"),
+        new Claim("languages", "English"),
+        new Claim("languages", "German"),
+        new Claim("languages", "Spanish")
     };
     var claimsIdentity = new ClaimsIdentity(claims, "Cookies");
     var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
@@ -73,11 +44,12 @@ app.Map("/", (HttpContext context) =>
 {
     // similar var username = context.User.Identity.Name
     var username = context.User.FindFirst(ClaimTypes.Name);
-    var phone = context.User.FindFirst(ClaimTypes.MobilePhone);
-    var company = context.User.FindFirst("company");
-    var age = context.User.FindFirst("age");
-    return $"Name: {username?.Value}\nPhone: {phone?.Value}\n" +
-    $"Company: {company?.Value}\nAge: {age?.Value}";
+    var languages = context.User.FindAll("languages");
+    // concatenate the claims list into a string
+    var languagesToString = "";
+    foreach(var l in languages)
+        languagesToString = $"{languagesToString} {l.Value}";
+    return $"Name: {username?.Value}\nLanguages: {languagesToString}";
 });
 
 app.Run();
