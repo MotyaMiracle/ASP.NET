7c25d723e365d32991828f081d05f49525623779
MotyaMiracle <motyamiracle@gmail.com>
2023-02-03 12:15:26 +0300

    Управление объектами Claim

--- Training/Program.cs
+++ Training/Program.cs
@@ -16,14 +16,51 @@ var app = builder.Build();
 
 app.UseAuthentication();
 
-
-app.MapGet("/login/{username}", async (string username,HttpContext context) =>
+// Add age
+app.MapGet("/addage", async (HttpContext context) =>
 {
-    var claims = new List<Claim> { new Claim(ClaimTypes.Name, username) };
-    var claimIdentity = new ClaimsIdentity(claims, "Cookies");
-    var claimPrincipal = new ClaimsPrincipal(claimIdentity);
-    await context.SignInAsync(claimPrincipal);
-    return $"Установлено имя {username}";
+    if (context.User.Identity is ClaimsIdentity claimsIdentity)
+    {
+        claimsIdentity.AddClaim(new Claim("age", "37"));
+        var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+        await context.SignInAsync(claimsPrincipal);
+    }
+    return Results.Redirect("/");
+});
+
+// delete phone
+app.MapGet("/removephone", async (HttpContext context) =>
+{
+    if (context.User.Identity is ClaimsIdentity claimsIdentity)
+    {
+        var phoneClaim = claimsIdentity.FindFirst(ClaimTypes.MobilePhone);
+        // if the claim is successfully deleted
+        if (claimsIdentity.TryRemoveClaim(phoneClaim))
+        {
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+            await context.SignInAsync(claimsPrincipal);
+        }
+    }
+    return Results.Redirect("/");
+});
+
+app.MapGet("/login", async (HttpContext context) =>
+{
+    var username = "Tom";
+    var company = "Microsoft";
+    var phone = "+12345678901";
+
+    var claims = new List<Claim>
+    {
+        new Claim(ClaimTypes.Name, username),
+        new Claim("company", company),
+        new Claim(ClaimTypes.MobilePhone, phone)
+    };
+    var claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+    var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+    await context.SignInAsync(claimsPrincipal);
+    return Results.Redirect("/");
+
 });
 
 app.MapGet("/logout", async (HttpContext context) =>
@@ -34,11 +71,13 @@ app.MapGet("/logout", async (HttpContext context) =>
 
 app.Map("/", (HttpContext context) =>
 {
-    var user = context.User.Identity;
-    if(user is not null && user.IsAuthenticated)
-        return $"UserName: {user.Name}";
-    else
-        return "Пользователь НЕ аутентифицирован";
+    // similar var username = context.User.Identity.Name
+    var username = context.User.FindFirst(ClaimTypes.Name);
+    var phone = context.User.FindFirst(ClaimTypes.MobilePhone);
+    var company = context.User.FindFirst("company");
+    var age = context.User.FindFirst("age");
+    return $"Name: {username?.Value}\nPhone: {phone?.Value}\n" +
+    $"Company: {company?.Value}\nAge: {age?.Value}";
 });
 
 app.Run();
